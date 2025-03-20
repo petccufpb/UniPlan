41bdea9290480161912a8e3bdc7faceebfaf7bd7
LPfontes <82241012+LPfontes@users.noreply.github.com>
2025-03-20 12:22:29 -0300

    Alteração no método de divisão das disciplinas

--- frontend/src/app/planejador/page.tsx
+++ frontend/src/app/planejador/page.tsx
@@ -14,15 +14,13 @@ export default function Planejador() {
 		3: 'Optativa',
 		4: 'Complementar Obrigatória', // Índice 4 é a mesma categoria de índice 1
 	}
-
-	// Reduz a lista de disciplinas, agrupando-as por categoria.
-	// O resultado é um objeto onde as chaves são os nomes das categorias e os valores são arrays de disciplinas pertencentes a essa categoria.
-	const disciplinesByCategory = disciplines.reduce<{ [key: string]: typeof disciplines }>((acc, discipline) => {
-		const categoryName = NomeTipoDisciplina[discipline.category] || 'Categoria Desconhecida'
-		acc[categoryName] = acc[categoryName] ? [...acc[categoryName], discipline] : [discipline]
+	// Agrupa disciplinas por categoria
+	const disciplinesByCategory = disciplines.reduce<Map<number, (typeof disciplines)[0][]>>((acc, discipline) => {
+		const category = discipline.category
+		if (!acc.has(category)) acc.set(category, [])
+		acc.get(category)?.push(discipline)
 		return acc
-	}, {})
-
+	}, new Map())
 	return (
 		<main className="flex flex-col gap-12">
 			{/* Renderização dos períodos */}
@@ -38,26 +36,22 @@ export default function Planejador() {
 			<div className="w-full bg-gray-900 p-4 rounded-lg shadow-md">
 				<h2 className="text-xl font-semibold text-white mb-2">Disciplinas</h2>
 				<div className="max-h-80 overflow-y-auto p-2">
-					{Object.entries(disciplinesByCategory).map(
-						(
-							[category, disciplines], // map das categorias
-						) => (
-							<div key={category} className="mb-4">
-								<h2 className="text-xl font-bold text-white mb-2">{category}</h2>
-								<div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-3">
-									{disciplines.map(discipline => (
-										<Card
-											key={discipline.id}
-											className="flex-1 min-w-[150px] bg-gray-800 text-white px-6 py-4 text-lg text-center shadow-md"
-											variant="outlined"
-										>
-											{discipline.name}
-										</Card>
-									))}
-								</div>
+					{Array.from(disciplinesByCategory.entries()).map(([category, disciplines]) => (
+						<div key={category} className="mb-4">
+							<h2 className="text-xl font-bold text-white mb-2">{NomeTipoDisciplina[category] || `Categoria ${category}`}</h2>
+							<div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-3">
+								{disciplines.map(discipline => (
+									<Card
+										key={discipline.id}
+										className="flex-1 min-w-[150px] bg-gray-800 text-white px-6 py-4 text-lg text-center shadow-md"
+										variant="outlined"
+									>
+										{discipline.name}
+									</Card>
+								))}
 							</div>
-						),
-					)}
+						</div>
+					))}
 				</div>
 			</div>
 		</main>
